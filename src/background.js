4f75911af71221330ba502230a96c5ded448e2ac
Jonathan Fernandez <jonathan.f@thoughtworks.com>
2020-09-12 21:56:03 +0200

    linter enabled and fixes applied

--- src/background.js
+++ src/background.js
@@ -1,146 +1,142 @@
-var furl = [];
-var ftname="";
-var isActive=false;
+var furl = []
+var ftname = ''
+var isActive = false
 
-loadDataFromStore();
-reloadListener();
+loadDataFromStore()
+reloadListener()
 
-function loadDataFromStore(){
-    chrome.storage.sync.get(function(result) {
-        isActive = result.active
-        ftname = result.ftname
-        furl = result.furl});
+function loadDataFromStore () {
+  chrome.storage.sync.get(function (result) {
+    isActive = result.active
+    ftname = result.ftname
+    furl = result.furl
+  })
 }
 
-function changeIcon(status, tabId)
-{
-    if(tabId === undefined || tabId < 0){
-        return;
-    }
+function changeIcon (status, tabId) {
+  if (tabId === undefined || tabId < 0) {
+    return
+  }
 
-    if(status==="On"){
-        chrome.pageAction.setIcon({
-            tabId: tabId,
-            path : {
-                "16": "images/on16.png",
-                "32": "images/on32.png",
-                "128": "images/on128.png"
-            }
-          });
-    }
-    else{
-        chrome.pageAction.setIcon({
-            tabId: tabId,
-            path : {
-                "16": "images/off16.png",
-                "32": "images/off32.png",
-                "128": "images/off128.png"
-            }
-          });
-    }
+  if (status === 'On') {
+    chrome.pageAction.setIcon({
+      tabId: tabId,
+      path: {
+        16: 'images/on16.png',
+        32: 'images/on32.png',
+        128: 'images/on128.png'
+      }
+    })
+  } else {
+    chrome.pageAction.setIcon({
+      tabId: tabId,
+      path: {
+        16: 'images/off16.png',
+        32: 'images/off32.png',
+        128: 'images/off128.png'
+      }
+    })
+  }
 }
 
-function setTitle(newTitle, tabId){
-    if(tabId === undefined || tabId < 0){
-        return;
-    }
-    chrome.pageAction.setTitle({tabId, title:newTitle});
+function setTitle (newTitle, tabId) {
+  if (tabId === undefined || tabId < 0) {
+    return
+  }
+  chrome.pageAction.setTitle({ tabId, title: newTitle })
 }
 
-function listener(details)
-{
-    if(!isActive){
-        const msg = "Extension Not active";
-        console.log("----------" + msg + "----------");
-        changeIcon("Off", details.tabId);
-        setTitle(msg,details.tabId);
-        return;
-    }
-    if(!furl || !ftname){
-        const msg = "Extension Not fully configured";
-        console.log("----------" + msg + "----------");
-        changeIcon("Off", details.tabId);
-        setTitle(msg,details.tabId);
-        return;
-    }
+function listener (details) {
+  if (!isActive) {
+    const msg = 'Extension Not active'
+    console.log('----------' + msg + '----------')
+    changeIcon('Off', details.tabId)
+    setTitle(msg, details.tabId)
+    return
+  }
+  if (!furl || !ftname) {
+    const msg = 'Extension Not fully configured'
+    console.log('----------' + msg + '----------')
+    changeIcon('Off', details.tabId)
+    setTitle(msg, details.tabId)
+    return
+  }
 
-    if(furl.every((url) => !details.url.includes(url))){
-        const msg = "None of the urls match";
-        console.log("----------" + msg + "----------");
-        return;
-    } 
-        
-    if(details.url.endsWith(ftname) || details.url.includes(ftname + '&')){
-        const msg = "FT already present";
-        console.log("----------" + msg + "----------");
-        changeIcon("On", details.tabId);
-        setTitle(msg,details.tabId);
-        return;
-    }
+  if (furl.every((url) => !details.url.includes(url))) {
+    const msg = 'None of the urls match'
+    console.log('----------' + msg + '----------')
+    return
+  }
 
-    var url = details.url + 
-        (details.url.indexOf("?") == -1 
-            ? "?" + ftname 
-            : "&" + ftname)
+  if (details.url.endsWith(ftname) || details.url.includes(ftname + '&')) {
+    const msg = 'FT already present'
+    console.log('----------' + msg + '----------')
+    changeIcon('On', details.tabId)
+    setTitle(msg, details.tabId)
+    return
+  }
 
-    console.log("url--> " + url);''             
-    changeIcon("On", details.tabId);
-    setTitle("Feature Toogle On",details.tabId);
-    return {redirectUrl:url};
+  var url = details.url +
+        (details.url.indexOf('?') === -1
+          ? '?' + ftname
+          : '&' + ftname)
+
+  console.log('url--> ' + url)
+  changeIcon('On', details.tabId)
+  setTitle('Feature Toogle On', details.tabId)
+  return { redirectUrl: url }
 }
 
-function reloadListener(){
-    if(chrome.webRequest.onBeforeRequest.hasListener(listener)){
-        chrome.webRequest.onBeforeRequest.removeListener(listener);
-        console.log('listener removed');
-    }
+function reloadListener () {
+  if (chrome.webRequest.onBeforeRequest.hasListener(listener)) {
+    chrome.webRequest.onBeforeRequest.removeListener(listener)
+    console.log('listener removed')
+  }
 
-    chrome.webRequest.onBeforeRequest.addListener(
-        listener,
-        {
-            urls: [
-                "<all_urls>"
-            ],
-            types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-        },
-        ["blocking"]
-    );
-    console.log('listener added');
-}
-
-//Show the extension in all pages
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { urlMatches: '//*/*' },      })
+  chrome.webRequest.onBeforeRequest.addListener(
+    listener,
+    {
+      urls: [
+        '<all_urls>'
       ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+      types: ['main_frame', 'sub_frame', 'stylesheet', 'script', 'image', 'object', 'xmlhttprequest', 'other']
+    },
+    ['blocking']
+  )
+  console.log('listener added')
+}
 
-//Track changes in Storage
-  chrome.storage.onChanged.addListener(function(changes, namespace) {
-    for (var key in changes) {
-        var storageChange = changes[key];
-        console.log('Storage key "%s" in namespace "%s" changed. ' +
+// Show the extension in all pages
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({ pageUrl: { urlMatches: '//*/*' } })
+    ],
+    actions: [new chrome.declarativeContent.ShowPageAction()]
+  }])
+})
+
+// Track changes in Storage
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (var key in changes) {
+    var storageChange = changes[key]
+    console.log('Storage key "%s" in namespace "%s" changed. ' +
                     'Old value was "%s", new value is "%s".',
-                    key,
-                    namespace,
-                    storageChange.oldValue,
-                    storageChange.newValue);
-    }
-    loadDataFromStore();
-    reloadListener();
-  });
+    key,
+    namespace,
+    storageChange.oldValue,
+    storageChange.newValue)
+  }
+  loadDataFromStore()
+  reloadListener()
+})
 
-
-chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        reloadListener();
-        console.log("This is a first install!");
-    }else if(details.reason == "update"){
-        reloadListener();
-        var thisVersion = chrome.runtime.getManifest().version;
-        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
-    }
-});
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason === 'install') {
+    reloadListener()
+    console.log('This is a first install!')
+  } else if (details.reason === 'update') {
+    reloadListener()
+    var thisVersion = chrome.runtime.getManifest().version
+    console.log('Updated from ' + details.previousVersion + ' to ' + thisVersion + '!')
+  }
+})
--- src/popup.js
+++ src/popup.js
@@ -1,58 +1,56 @@
-document.getElementById("addUrlBtn").addEventListener("click", (e) => addUrlPattern(e));
-document.getElementById("saveBtn").addEventListener("click", (e) => save());
-document.addEventListener("DOMContentLoaded", onLoad);
+document.getElementById('addUrlBtn').addEventListener('click', (e) => addUrlPattern(e))
+document.getElementById('saveBtn').addEventListener('click', (e) => save())
+document.addEventListener('DOMContentLoaded', onLoad)
 
-function onLoad() {
-  chrome.storage.sync.get(function(result) {
-    document.getElementById("isActive").checked = result.active;
-    document.getElementById("ftname").value = (result.ftname==undefined) ? "ft=test" : result.ftname;  
+function onLoad () {
+  chrome.storage.sync.get(function (result) {
+    document.getElementById('isActive').checked = result.active
+    document.getElementById('ftname').value = (result.ftname === undefined) ? 'ft=test' : result.ftname
 
-    if(result.furl==undefined){
-      document.getElementById("furl").value =  "https://www.example.com";
+    if (result.furl === undefined) {
+      document.getElementById('furl').value = 'https://www.example.com'
+    } else {
+      document.getElementById('furl').value = result.furl.shift()
+      result.furl.forEach(element => addUrlPattern(null, element))
     }
-    else{
-      document.getElementById("furl").value = result.furl.shift();
-      result.furl.forEach(element => addUrlPattern(null, element));
-    }
-  });
+  })
 }
 
-function save(){
+function save () {
+  var urlNodes = document.getElementById('urlPanel')
+    .querySelectorAll('.css-input')
+  var urls = Array.from(urlNodes)
+    .map(input => input.value)
 
-  var urlNodes = document.getElementById("urlPanel")
-                    .querySelectorAll(".css-input");
-  var urls = Array.from(urlNodes)    
-                    .map(input => input.value); 
-
-  var ftname = document.getElementById("ftname").value;
-  var isActive = document.getElementById("isActive").checked;
+  var ftname = document.getElementById('ftname').value
+  var isActive = document.getElementById('isActive').checked
 
   chrome.storage.sync.set({
-    'furl': urls,
-    'ftname': ftname,
-    'active': isActive
-  }, function() {});
-}
-  
-function removeUrlPattern(clickEvent) {
-  clickEvent.preventDefault();
-  var removeButton = clickEvent.target;
-  var urlPatternInput = removeButton.previousSibling;
-  document.getElementById("urlPanel").removeChild(removeButton);
-  document.getElementById("urlPanel").removeChild(urlPatternInput);
+    furl: urls,
+    ftname: ftname,
+    active: isActive
+  }, function () {})
 }
 
-function addUrlPattern(clickEvent, value){
-  clickEvent?.preventDefault();
-  
-  var newInput = document.createElement("INPUT")
-  newInput.setAttribute("type", "text");
-  newInput.className = "css-input";
-  newInput.value = value || "";
+function removeUrlPattern (clickEvent) {
+  clickEvent.preventDefault()
+  var removeButton = clickEvent.target
+  var urlPatternInput = removeButton.previousSibling
+  document.getElementById('urlPanel').removeChild(removeButton)
+  document.getElementById('urlPanel').removeChild(urlPatternInput)
+}
 
-  var newBtn = document.createElement("button");
-  newBtn.className ="rmUrlBtn";
-  newBtn.addEventListener("click", (e) => removeUrlPattern(e)); 
-  document.getElementById("urlPanel").appendChild(newInput);
-  document.getElementById("urlPanel").appendChild(newBtn);
+function addUrlPattern (clickEvent, value) {
+  clickEvent?.preventDefault()
+
+  var newInput = document.createElement('INPUT')
+  newInput.setAttribute('type', 'text')
+  newInput.className = 'css-input'
+  newInput.value = value || ''
+
+  var newBtn = document.createElement('button')
+  newBtn.className = 'rmUrlBtn'
+  newBtn.addEventListener('click', (e) => removeUrlPattern(e))
+  document.getElementById('urlPanel').appendChild(newInput)
+  document.getElementById('urlPanel').appendChild(newBtn)
 }
