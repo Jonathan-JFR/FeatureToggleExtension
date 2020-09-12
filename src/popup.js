fcc9e25fc179a328868809bcf0c518b9d05479b5
Jonathan Fernandez <jonathan.f@thoughtworks.com>
2020-09-12 16:19:44 +0200

    Add/Remove url fields

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,29 @@
+document.getElementById("addUrlBtn").addEventListener("click", (e) => addUrlPattern(e));
+
+document.addEventListener("DOMContentLoaded", onLoad);
+
+  
+function removeUrlPattern(clickEvent) {
+  clickEvent.preventDefault();
+  var removeButton = clickEvent.target;
+  var urlPatternInput = removeButton.previousSibling;
+  document.getElementById("urlPanel").removeChild(removeButton);
+  document.getElementById("urlPanel").removeChild(urlPatternInput);
+}
+
+function addUrlPattern(clickEvent) {
+  clickEvent.preventDefault();
+  var newInput = document.createElement("INPUT");
+  newInput.setAttribute("type", "text");
+  newInput.className = "css-input";
+  var newBtn = document.createElement("button");
+  newBtn.className ="rmUrlBtn";
+  newBtn.addEventListener("click", (e) => removeUrlPattern(e)); 
+  document.getElementById("urlPanel").appendChild(newInput);
+  document.getElementById("urlPanel").appendChild(newBtn);
+
+}
+
 document.getElementById("saveBtn").onclick = function() {
     var furl = document.getElementById("furl").value;
     var ftname = document.getElementById("ftname").value;
@@ -8,12 +34,11 @@ document.getElementById("saveBtn").onclick = function() {
     chrome.storage.sync.set({'active': isActive}, function() {});
   }
 
-document.addEventListener("DOMContentLoaded", onLoad);
-  
 function onLoad() {
   chrome.storage.sync.get(function(result) {
     document.getElementById("furl").value = (result.furl==undefined) ? "www.example.com" : result.furl;
     document.getElementById("ftname").value = (result.ftname==undefined) ? "ft=test" : result.ftname;  
     document.getElementById("isActive").checked = result.active;
   });
+
 }
