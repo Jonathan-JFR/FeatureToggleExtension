8fd01173e5ff66fccb4d8a688df7a59dcd0c8428
Jonathan Fernandez <jonathan.f@thoughtworks.com>
2020-09-19 16:31:50 +0200

    Localization EN/ES

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,23 @@
+
+// Credit for the localizeHtmlPage function: https://stackoverflow.com/a/25612056/81076
+function localizeHtmlPage () {
+  var objects = document.getElementsByTagName('html')
+  for (var j = 0; j < objects.length; j++) {
+    var obj = objects[j]
+
+    var valStrH = obj.innerHTML.toString()
+    var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function (match, v1) {
+      return v1 ? chrome.i18n.getMessage(v1) : ''
+    })
+
+    if (valNewH !== valStrH) {
+      obj.innerHTML = valNewH
+    }
+  }
+}
+
+localizeHtmlPage()
+
 document.getElementById('addUrlBtn').addEventListener('click', (e) => addUrlPattern(e))
 document.getElementById('saveBtn').addEventListener('click', (e) => save())
 document.addEventListener('DOMContentLoaded', onLoad)
